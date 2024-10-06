c1a0785487d4a6202773c2e2b6034ff90b7b10dd
athiravasavan <aathivaas@gmail.com>
2024-10-06 21:07:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import LoginPage from './LoginPage';
 import OTPInput from './OTPInput';
 import ForgotPassword from './ForgotPassword';
 import DetailedSignUp from './DetailedSignUp';
+import LawyersList from './LawyersList'; // Default import
+import LawyerProfile from './LawyerProfile'; // Default import
 import './App.css'; // Optional: Your global styles
 
 const App = () => {
@@ -14,13 +16,21 @@ const App = () => {
         <Router>
             <div className="App">
                 <Routes>
+                    {/* Define your app's routes */}
                     <Route path="/" element={<WelcomePage />} />
                     <Route path="/citizen-signup" element={<CitizenSignUp />} />
                     <Route path="/otp-input" element={<OTPInput />} />
                     <Route path="/lawfirm-signup" element={<LawFirmSignUp />} />
                     <Route path="/login" element={<LoginPage />} />
-                    <Route path="/detailed-signup" element={<DetailedSignUp />} /> 
+                    <Route path="/detailed-signup" element={<DetailedSignUp />} />
                     <Route path="/forgot-password" element={<ForgotPassword />} />
+
+                    {/* List of registered lawyers */}
+                    <Route path="/lawyers" element={<LawyersList />} />
+
+                    {/* Lawyer profile: View and Edit based on the URL */}
+                    <Route path="/lawyer/:id/view" element={<LawyerProfile isEdit={false} />} />
+                    <Route path="/lawyer/:id/edit" element={<LawyerProfile isEdit={true} />} />
                 </Routes>
             </div>
         </Router>
